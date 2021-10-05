7958b49f3d3e3dbc1c6b91d3cc163b9093335d09
unknown <ilham>
2021-10-06 01:09:02 +0400

    Fixes

--- src/auth/dto/registration-customer.dto.ts
+++ src/auth/dto/registration-customer.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail } from "class-validator";
+import { IsEmail, IsEmpty, IsString } from "class-validator";
 import { ApiProperty } from "@nestjs/swagger";
 
 export class RegistrationCustomerDto {
@@ -6,4 +6,12 @@ export class RegistrationCustomerDto {
   @ApiProperty({ example: "ilham.pirm2@gmail.com", description: "Почта, Хардкод-смс: 123456", required: false })
   @IsEmail()
   email: string;
+
+  @ApiProperty({ example: "+994503190044", description: "Почта, Хардкод-смс: 123456", required: false })
+  @IsString()
+  phone: string;
+
+  @ApiProperty({ example: "Ilham564/", description: "Почта, Хардкод-смс: 123456", required: false })
+  @IsEmpty()
+  password: string;
 }
