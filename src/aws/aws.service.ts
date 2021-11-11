fa65c0d8bfe418f7c122f96b75e9419b20d88edb
unknown <ilham>
2021-11-11 20:33:12 +0400

    Fixes

--- src/aws/aws.service.ts
+++ src/aws/aws.service.ts
@@ -5,16 +5,25 @@ import { S3 } from "aws-sdk";
 export class AwsService {
 
   async uploadPublicFile(file) {
-    const s3 = new S3();
+
+    const s3 = new S3({
+      endpoint: process.env.AWS_LINK,
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+      }
+    });
+
     const uploadResult = await s3.upload({
       Bucket: process.env.AWS_PUBLIC_BUCKET_NAME,
       Body: file.buffer,
       Key: file.originalname,
       ACL: "public-read"
     }).promise();
+
     return {
       key: uploadResult.Key,
       url: uploadResult.Location
-    };
+    }
   }
 }
